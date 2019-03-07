f18a9e8cfb626f844f74eb0523c6eb29d74b2846
Fernando Zavalia <zavaliafernando@gmail.com>
2019-03-06 22:38:24 -0300

    Add crud repository

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import morgan from 'morgan';
-
-const app = express()
-
-app.use(bodyParser.json())
-
-app.use(morgan('tiny'))
-
-app.listen(8000, () => {
-  console.log('Listening on port 8000')
-})
+import './web'
---
+++ src/repository/CRUD.ts
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose'
+
+class CRUD {
+
+  constructor(private model: mongoose.Model<mongoose.Document, {}>) { }
+
+  find = async (id: string) =>
+    await this.model
+      .findById(id)
+      .orFail()
+      .lean()
+
+  list = async (page: number, perPage: number) =>
+    await this.model
+      .find({})
+      .skip((page - 1) * perPage)
+      .limit(perPage)
+      .orFail()
+      .lean()
+
+  create = async (data: any) =>
+    await this.model
+      .create(data)
+      .then(res => res.toObject())
+
+  update = async (id: string, data: any) =>
+    await this.model
+      .findByIdAndUpdate(id, data, { new: true })
+      .orFail()
+      .lean()
+
+  delete = async (id: string) =>
+    await this.model
+      .findByIdAndDelete(id)
+      .orFail()
+      .lean()
+}
+
+export default CRUD
