606a3bdbb91ea3e7645c02042eaa5eaaa5bb3078
Fernando Zavalia <zavaliafernando@gmail.com>
2019-03-10 19:38:07 -0300

    Reformat code

--- src/web/handlers/BudgetsRequestHandler.ts
+++ src/web/handlers/BudgetsRequestHandler.ts
@@ -6,33 +6,25 @@ class BudgetsRequestHandler extends RequestHandler {
 
   create = async (req: Request, res: Response) => {
     try {
-
       const created = await Budget.create(req.body);
-
-      res.send(created);
+      res.send(this.mapDocument(created));
     }
     catch (e) {
-
       res.status(500);
       res.send(e);
-
     }
   };
 
   list = async (req: Request, res: Response) => {
     try {
-
       const findQuery = Budget.find(this.filters(req, { exact: ['year', 'month'] }))
       const paginateQuery = this.paginate(req, findQuery)
-      const budgets = await paginateQuery
-
-      res.send(budgets);
+      const budgets = await paginateQuery.lean()
+      res.send(budgets.map(this.mapJSON));
     }
     catch (e) {
-
       res.status(500);
       res.send(e);
-
     }
   }
 }
--- src/web/handlers/UsersRequestHandler.ts
+++ src/web/handlers/UsersRequestHandler.ts
@@ -6,49 +6,36 @@ class UsersRequestHandler extends RequestHandler {
 
   create = async (req: Request, res: Response) => {
     try {
-
       const created = await User.create(req.body);
       res.send(this.mapDocument(created));
-
     }
     catch (e) {
-
       res.status(500);
       res.send(e);
-
     }
   };
 
   show = async (req: Request, res: Response) => {
     try {
-
       const user = await User.findById(req.params.id).orFail().lean()
       res.send(this.mapJSON(user))
-
     }
     catch (e) {
-
       res.status(500);
       res.send(e);
-
     }
   }
 
   list = async (req: Request, res: Response) => {
     try {
-
       const findQuery = User.find(this.filters(req, { exact: ['name'] }))
       const paginateQuery = this.paginate(req, findQuery)
       const users = await paginateQuery.lean()
-
       res.send(users.map(this.mapJSON))
-
     }
     catch (e) {
-
       res.status(500);
       res.send(e);
-
     }
   }
 }
