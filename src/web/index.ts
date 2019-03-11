09ffa8bf5591583d3981315673862823285c94d4
Fernando Zavalia <zavaliafernando@gmail.com>
2019-03-11 20:51:52 -0300

    Refactor bindings

--- src/web/index.ts
+++ src/web/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { Router, Express } from 'express'
 import bodyParser from 'body-parser'
 import morgan from 'morgan';
 import cors from 'cors';
@@ -45,29 +45,38 @@ export default async (config: Config) => {
 
   app.get('/', (_, res) => res.send('ok'))
 
-  const usersRequestHandler = new UsersRequestHandler()
+  bindUsersRequestHandlerToApp(app)
+  bindBudgetsRequestHandlerToApp(app)
+  bindRestaurantsRequestHandlerToApp(app)
+  bindExpensesRequestHandlerToApp(app)
 
+  app.listen(port, () => console.log(`Listening on port ${port}`))
+}
+
+const bindUsersRequestHandlerToApp = (app: Express) => {
+  const usersRequestHandler = new UsersRequestHandler()
   app.get('/users', usersRequestHandler.list)
   app.get('/users/:id', usersRequestHandler.show)
   app.post('/users', usersRequestHandler.create)
+}
 
+const bindRestaurantsRequestHandlerToApp = (app: Express) => {
   const restaurantRequestHandler = new RestaurantsRequestHandler()
-
   app.get('/restaurants', restaurantRequestHandler.list)
   app.post('/restaurants', restaurantRequestHandler.create)
+}
 
+const bindBudgetsRequestHandlerToApp = (app: Express) => {
   const budgetsRequestHandler = new BudgetsRequestHandler()
-
   app.get('/budgets', budgetsRequestHandler.list)
   app.post('/budgets', budgetsRequestHandler.create)
+}
 
+const bindExpensesRequestHandlerToApp = (app: Express) => {
   const expensesRequestHandler = new ExpensesRequestHandler()
-
   app.get('/expenses/year/:year/month/:month', expensesRequestHandler.listForMonth)
   app.get('/expenses/year/:year/month/:month/spent', expensesRequestHandler.spentOnMonth)
   app.get('/expenses/year/:year', expensesRequestHandler.listForYear)
   app.get('/expenses/year/:year/spent', expensesRequestHandler.spentOnYear)
   app.post('/expenses', expensesRequestHandler.create)
-
-  app.listen(port, () => console.log(`Listening on port ${port}`))
 }
